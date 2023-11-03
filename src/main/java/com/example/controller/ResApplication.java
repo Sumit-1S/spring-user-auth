f38320e870decd2bb36eb8fade72b0d8ef5a6c0a
= <=>
2023-11-03 10:45:40 -0700

    Initial Commit

--- src/main/java/com/example/controller/ResApplication.java
+++ src/main/java/com/example/controller/ResApplication.java
@@ -5,10 +5,13 @@ import java.util.List;
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,7 +21,6 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.entity.User;
 import com.example.service.UserService;
-import com.example.utils.EncryptPassword;
 import com.model.UserRegisterRequest;
 import com.model.UserLoginRequest;
 import com.model.UserPasswordResetRequest;
@@ -31,30 +33,30 @@ public class ResApplication {
 	@Autowired
 	private UserService service;
 	
-	EncryptPassword encoder = new EncryptPassword();
 	
 	@PostMapping("/register")
 	@ResponseBody
-	public String registerValidate(@ModelAttribute UserRegisterRequest user) throws Exception{
+	public ResponseEntity<String> registerValidate(@RequestBody UserRegisterRequest user) throws Exception{
+		System.out.println(user);
 		if(user.getPassword().equals(user.getConfirmPassword())) {
-			return service.addUser(new User(user.getUsername(),encoder.encryptPassword(user.getPassword()),user.getUserType()));
+			return new ResponseEntity<>(service.addUser(new User(user.getUsername(),user.getPassword(),user.getUserType())),HttpStatus.OK);
 		}
-		return "Passwords Don't Match!!!";
+		return new ResponseEntity<>("Passwords Don't Match!!!",HttpStatus.OK);
 	}
 	
-	@PostMapping("/forgetpassword")
+	@PutMapping("/forgetpassword")
 	@ResponseBody
 	@Transactional
-	public String forgetPassword(@ModelAttribute UserPasswordResetRequest user) throws Exception{
-		user.setPassword(encoder.encryptPassword(user.getPassword()));
-		user.setConfirmPassword(encoder.encryptPassword(user.getConfirmPassword()));
+	public String forgetPassword(@RequestBody UserPasswordResetRequest user) throws Exception{
+		user.setPassword(user.getPassword());
+		user.setConfirmPassword(user.getConfirmPassword());
 		return service.updatePassword(user);	
 	}
 	
 	@PostMapping("/login")
 	@ResponseBody
-	public Boolean login(@ModelAttribute UserLoginRequest user) throws Exception{
-		user.setPassword(encoder.encryptPassword(user.getPassword()));
+	public Boolean login(@RequestBody UserLoginRequest user) throws Exception{
+		user.setPassword(user.getPassword());
 		System.out.println(user.getPassword());
 		return service.verifyUser(user);	
 	}
